d8af67d482f525159d58cbfc4d71a27b257a459d
ERIC MUNIZ MENDONÇA <eric_mendonca@estudante.sesisenai.org.br>
2023-12-07 19:29:29 -0300

    feat: styling the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import { FiMenu } from "react-icons/fi";
+import { CgCross } from "react-icons/cg";
 import { Link } from 'react-router-dom';
 
 function App(){
@@ -12,7 +13,7 @@ function App(){
         <h2>Cantaris Coral</h2>
         {/* <img className='logo' src="https://www.imagensempng.com.br/wp-content/uploads/2021/08/04-32.png" alt="imagem-logo" /> */}
       </div>
-      <div><button><FiMenu  className='menu-button'/></button></div>
+      <div><button style={{cursor: "pointer"}}><FiMenu  className='menu-button'/></button></div>
     </div>
     <div className='container'>
       <div className='lista-content'>
@@ -22,9 +23,22 @@ function App(){
         </ul>
       </div>
       <div className='home-footer'>
+        <button className='footer-title' style={{cursor: "pointer"}}>
+          <h1 style={{color: "white"}}>Conheca a história do</h1>
+          <span style={{color: "red", fontSize: 45, fontWeight: 600}}>Cantaris Coral</span>
+        </button>
+
+        <div className='footer-cross'>
+          <CgCross color='white' size={45} className='footer-cross'/>
+        </div>
+
         <div className='footer-content'>
-          <div style={{color: 'white'}}>Instagram</div>
-          <div style={{color: 'white'}}>Facebook</div>
+          <div className='footer-icon'>
+            <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/instagram-white-icon.png" alt="" />
+          </div>
+          <div className='footer-icon'>
+            <img src="https://www.edigitalagency.com.au/wp-content/uploads/facebook-icon-white-png.png" alt="" />
+          </div>
         </div>
       </div>
     </div>
